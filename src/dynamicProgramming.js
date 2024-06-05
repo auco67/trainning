4b55439a8cd67c767b744985a2d18fa480dc0d51
auco67 <auco67@gmail.com>
2024-06-05 17:40:44 +0900

    Add code to sample code.

--- src/dynamicProgramming.js
+++ src/dynamicProgramming.js
@@ -175,3 +175,68 @@ function step6(){
     }
 }
 step6()
+
+/*
+ * STEP: 7 階段の上り方 1
+ *  整数 n が与えられます。階段を上るのに、1 歩で 1 段または 2 段を上ることができるとき、n 段の階段を上る方法は何通りあるでしょうか。
+ *  部分問題として、1 ~ n-1 段の階段を上る方法が何通りあるか、という問題を考えてみましょう。
+ *  n 段目に到達するには、n-1 段目から1段上る方法と、n-2 段目から2段上る方法の2種類が考えられます。
+ *  dp[n] を n 段の階段を上る方法の数とすれば、この関係は dp[n] = dp[n-1] + dp[n-2] で表すことが出来ます。
+ */
+function step7(){
+    var lines = ['3']
+    var n = Number.parseInt(lines[0])
+    var steps = []
+    steps.push(1)
+     
+    for(var i= 1; i<n; i++){
+        steps.push(i)  
+        if(i>=1){
+            var num = Number.parseInt(steps[i]) + Number.parseInt(steps[i-1])
+        }
+        if(i>=2){
+            num= Number.parseInt(steps[i]) + Number.parseInt(steps[i-2])
+            
+        }
+        steps[i] = num
+        
+    }
+    console.log('' + steps[n-1])
+
+}
+step7()
+
+/*
+ * STEP: 8 階段の上り方 2
+ */
+
+/*
+ * STEP: 9 【階段の上り方】階段の上り方 3
+ */
+
+/*
+ * STEP: 10 最安値を達成するには 1
+ *  八百屋にて、りんご1個が a 円で、りんご2個が b 円で売られています。
+ *  りんごの買い方を工夫したとき、n 個のりんごを手に入れるために必要な金額の最小値はいくらでしょうか。
+ *  なお、買い方を工夫した結果、買ったりんごが n+1 個以上になってもよいものとします。
+ *  部分問題として、りんご i 個 (1 ≦ i ≦ n-1) を買うのに必要な金額の最小値を求める問題を考えてみましょう。
+ */
+function step10(){
+    var lines = ['5 100 150']
+    var ary = lines[0].split(' ')
+    var n = Number.parseInt(ary[0])
+    var a = Number.parseInt(ary[1])
+    var b = Number.parseInt(ary[2])
+    var method = []
+    method.push(0)
+    method.push(a)
+    if( (n>=1 && n<=1000) && (a>=1 && a<=10000) && (b>=1 && b<=10000)){
+        if(a<b){
+            for(var i= 2; i<=n; i++){
+                method.push(Math.min(method[i-1]+a, method[i-2]+b))
+            }
+        }
+    }
+    console.log(method[n])
+}
+step10()
