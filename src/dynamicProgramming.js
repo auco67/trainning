fc8542822f747120c4053c34f83162ecc7eab14d
auco67 <auco67@gmail.com>
2024-06-06 22:04:03 +0900

    Add code to sample code.

--- src/dynamicProgramming.js
+++ src/dynamicProgramming.js
@@ -49,7 +49,7 @@ function step2() {
   result.push(0)
   result.push(x)
   for (var i = 2; i < 1001; i++) {
-      result.push(Number.parseInt(result[i - 1]) + d)
+    result.push(Number.parseInt(result[i - 1]) + d)
   }
   for (var i = 0; i < Q; i++) {
     var int = Number.parseInt(lines[i])
@@ -58,33 +58,33 @@ function step2() {
 }
 step2()
 
-/* 
+/*
  * STEP: 3 特殊な2項間漸化式 1
  *  整数 x, d_1, d_2, k が与えられます。
  *  次のように定められた数列の k 項目の値を出力してください。
- *   a_1 = x 
- *   a_n = a_{n-1} + d_1 (n が奇数のとき、n ≧ 3) 
+ *   a_1 = x
+ *   a_n = a_{n-1} + d_1 (n が奇数のとき、n ≧ 3)
  *   a_n = a_{n-1} + d_2 (n が偶数のとき)
  */
-function step3(){
-    var lines = ['5 -7 10 5']
-    var ary = lines[0].split(' ')
-    var x = Number.parseInt(ary[0])
-    var d_1 = Number.parseInt(ary[1])
-    var d_2 = Number.parseInt(ary[2])
-    var k = Number.parseInt(ary[3])
+function step3() {
+  var lines = ['5 -7 10 5']
+  var ary = lines[0].split(' ')
+  var x = Number.parseInt(ary[0])
+  var d_1 = Number.parseInt(ary[1])
+  var d_2 = Number.parseInt(ary[2])
+  var k = Number.parseInt(ary[3])
 
-    var result = []
-    result.push(0)
-    result.push(x)
-    for(var i=2; i<1001; i++){
-        if(i%2!=0){
-            result.push(Number.parseInt(result[i-1])+d_1)
-        }else{
-            result.push(Number.parseInt(result[i-1])+d_2)
-        }
+  var result = []
+  result.push(0)
+  result.push(x)
+  for (var i = 2; i < 1001; i++) {
+    if (i % 2 != 0) {
+      result.push(Number.parseInt(result[i - 1]) + d_1)
+    } else {
+      result.push(Number.parseInt(result[i - 1]) + d_2)
     }
-    console.log('STEP: 3 特殊な2項間漸化式 1 ' + result[k])
+  }
+  console.log('STEP: 3 特殊な2項間漸化式 1 ' + result[k])
 }
 step3()
 
@@ -92,33 +92,33 @@ step3()
  * STEP: 4 特殊な2項間漸化式 2
  *  整数 x, d_1, d_2, Q と Q 個の整数 k_1, k_2, ... , k_Q が与えられます。
  *  次のように定められた数列の k_i 項目の値を順に出力してください。
- *  a_1 = x 
- *  a_n = a_{n-1} + d_1 (n が奇数のとき、n ≧ 3) 
+ *  a_1 = x
+ *  a_n = a_{n-1} + d_1 (n が奇数のとき、n ≧ 3)
  *  a_n = a_{n-1} + d_2 (n が偶数のとき)
  */
-function step4(){
-    var lines = ['3 7 -4', 5, 1, 2, 3, 4, 10]
-    var ary = lines[0].split(' ')
-    var x = Number.parseInt(ary[0])
-    var d_1 = Number.parseInt(ary[1])
-    var d_2 = Number.parseInt(ary[2])
-    lines.shift()
-    var Q = Number.parseInt(lines[0])
-    lines.shift()
-    var result = []
-    result.push(0)
-    result.push(x)
-    for(var i=2; i<1001; i++){
-        if(i%2!=0){
-            result.push(Number.parseInt(result[i-1])+d_1)
-        }else{
-            result.push(Number.parseInt(result[i-1])+d_2)
-        }
+function step4() {
+  var lines = ['3 7 -4', 5, 1, 2, 3, 4, 10]
+  var ary = lines[0].split(' ')
+  var x = Number.parseInt(ary[0])
+  var d_1 = Number.parseInt(ary[1])
+  var d_2 = Number.parseInt(ary[2])
+  lines.shift()
+  var Q = Number.parseInt(lines[0])
+  lines.shift()
+  var result = []
+  result.push(0)
+  result.push(x)
+  for (var i = 2; i < 1001; i++) {
+    if (i % 2 != 0) {
+      result.push(Number.parseInt(result[i - 1]) + d_1)
+    } else {
+      result.push(Number.parseInt(result[i - 1]) + d_2)
     }
-    for(i=0; i<Q; i++){
-        var index = Number.parseInt(lines[i])
-        console.log('STEP: 4 特殊な2項間漸化式 2' + result[index])
-    }
+  }
+  for (i = 0; i < Q; i++) {
+    var index = Number.parseInt(lines[i])
+    console.log('STEP: 4 特殊な2項間漸化式 2' + result[index])
+  }
 }
 step4()
 
@@ -126,53 +126,52 @@ step4()
  * STEP: 5 3項間漸化式 1
  *  整数 k が与えられます。次のように定められた数列の k 項目の値を出力してください。
  *  ちなみに、これはフィボナッチ数列と呼ばれる有名な数列です。
- *  a_1 = 1 
- *  a_2 = 1 
+ *  a_1 = 1
+ *  a_2 = 1
  *  a_n = a_{n-2} + a_{n-1} (n ≧ 3)
  */
-function step5(){
-    var lines = ['7']
-    var k = Number.parseInt(lines[0])
-    var x_1 = 1
-    var x_2 = 1
-    var result = []
-    result.push(0)
-    result.push(x_1)
-    result.push(x_2)
-    for(var i=3; i<1002; i++){
-        result.push(result[i-2]+ result[i-1])
-    }
-    console.log('STEP: 5 3項間漸化式 1 ' + result[k])
-
+function step5() {
+  var lines = ['7']
+  var k = Number.parseInt(lines[0])
+  var x_1 = 1
+  var x_2 = 1
+  var result = []
+  result.push(0)
+  result.push(x_1)
+  result.push(x_2)
+  for (var i = 3; i < 1002; i++) {
+    result.push(result[i - 2] + result[i - 1])
+  }
+  console.log('STEP: 5 3項間漸化式 1 ' + result[k])
 }
 step5()
 
 /*
- * STEP: 6 【漸化式】 3項間漸化式 2 
+ * STEP: 6 【漸化式】 3項間漸化式 2
  *  整数 Q と Q 個の整数 k_1, k_2, ... , k_Q が与えられます。
  *  次のように定められた数列の k_i 項目の値を順に出力してください。
  *  ちなみに、これはフィボナッチ数列と呼ばれる有名な数列です。
- *  a_1 = 1 
- *  a_2 = 1 
+ *  a_1 = 1
+ *  a_2 = 1
  *  a_n = a_{n-2} + a_{n-1} (n ≧ 3)
  */
-function step6(){
-    var lines = [5, 1, 2, 3, 4, 3]
-    var x_1 = 1
-    var x_2 = 1
-    var Q = Number.parseInt(lines[0])
-    lines.shift()
-    var result = []
-    result.push(0)
-    result.push(x_1)
-    result.push(x_2)
-    for(var i=3; i<102; i++){
-        result.push(Number.parseInt(result[i-2])+ Number.parseInt(result[i-1]))
-    }
-    for(i=0; i<Q; i++){
-        var index = Number.parseInt(lines[i])
-        console.log('STEP: 6 【漸化式】 3項間漸化式 2  ' +result[index])
-    }
+function step6() {
+  var lines = [5, 1, 2, 3, 4, 3]
+  var x_1 = 1
+  var x_2 = 1
+  var Q = Number.parseInt(lines[0])
+  lines.shift()
+  var result = []
+  result.push(0)
+  result.push(x_1)
+  result.push(x_2)
+  for (var i = 3; i < 102; i++) {
+    result.push(Number.parseInt(result[i - 2]) + Number.parseInt(result[i - 1]))
+  }
+  for (i = 0; i < Q; i++) {
+    var index = Number.parseInt(lines[i])
+    console.log('STEP: 6 【漸化式】 3項間漸化式 2  ' + result[index])
+  }
 }
 step6()
 
@@ -183,26 +182,23 @@ step6()
  *  n 段目に到達するには、n-1 段目から1段上る方法と、n-2 段目から2段上る方法の2種類が考えられます。
  *  dp[n] を n 段の階段を上る方法の数とすれば、この関係は dp[n] = dp[n-1] + dp[n-2] で表すことが出来ます。
  */
-function step7(){
-    var lines = ['3']
-    var n = Number.parseInt(lines[0])
-    var steps = []
-    steps.push(1)
-     
-    for(var i= 1; i<n; i++){
-        steps.push(i)  
-        if(i>=1){
-            var num = Number.parseInt(steps[i]) + Number.parseInt(steps[i-1])
-        }
-        if(i>=2){
-            num= Number.parseInt(steps[i]) + Number.parseInt(steps[i-2])
-            
-        }
-        steps[i] = num
-        
-    }
-    console.log('' + steps[n-1])
+function step7() {
+  var lines = ['3']
+  var n = Number.parseInt(lines[0])
+  var steps = []
+  steps.push(1)
 
+  for (var i = 1; i < n; i++) {
+    steps.push(i)
+    if (i >= 1) {
+      var num = Number.parseInt(steps[i]) + Number.parseInt(steps[i - 1])
+    }
+    if (i >= 2) {
+      num = Number.parseInt(steps[i]) + Number.parseInt(steps[i - 2])
+    }
+    steps[i] = num
+  }
+  console.log('' + steps[n - 1])
 }
 step7()
 
@@ -221,23 +217,23 @@ step7()
  *  なお、買い方を工夫した結果、買ったりんごが n+1 個以上になってもよいものとします。
  *  部分問題として、りんご i 個 (1 ≦ i ≦ n-1) を買うのに必要な金額の最小値を求める問題を考えてみましょう。
  */
-function step10(){
-    var lines = ['5 100 150']
-    var ary = lines[0].split(' ')
-    var n = Number.parseInt(ary[0])
-    var a = Number.parseInt(ary[1])
-    var b = Number.parseInt(ary[2])
-    var method = []
-    method.push(0)
-    method.push(a)
-    if( (n>=1 && n<=1000) && (a>=1 && a<=10000) && (b>=1 && b<=10000)){
-        if(a<b){
-            for(var i= 2; i<=n; i++){
-                method.push(Math.min(method[i-1]+a, method[i-2]+b))
-            }
-        }
+function step10() {
+  var lines = ['5 100 150']
+  var ary = lines[0].split(' ')
+  var n = Number.parseInt(ary[0])
+  var a = Number.parseInt(ary[1])
+  var b = Number.parseInt(ary[2])
+  var method = []
+  method.push(0)
+  method.push(a)
+  if (n >= 1 && n <= 1000 && a >= 1 && a <= 10000 && b >= 1 && b <= 10000) {
+    if (a < b) {
+      for (var i = 2; i <= n; i++) {
+        method.push(Math.min(method[i - 1] + a, method[i - 2] + b))
+      }
     }
-    console.log('STEP: 10 最安値を達成するには 1 ' + method[n])
+  }
+  console.log('STEP: 10 最安値を達成するには 1 ' + method[n])
 }
 step10()
 
@@ -247,32 +243,32 @@ step10()
  *  りんごの買い方を工夫したとき、n 個のりんごを手に入れるために必要な金額の最小値はいくらでしょうか。
  *  なお、買い方を工夫した結果、買ったりんごが n+1 個以上になってもよいものとします。
  */
-function step11(){
-    var lines = ['4 110 200']
-    var ary = lines[0].split(' ')
-    var n = Number.parseInt(ary[0])
-    var a = Number.parseInt(ary[1])
-    var b = Number.parseInt(ary[2])
-    var method = []
-    method.push(0)
-    method.push(a)
-    if( (n>=1 && n<=1000) && (a>=1 && a<=10000) && (b>=1 && b<=10000)){
-        if(a<b){
-            for(var i=2; i<n+5; i++){
-                method.push(0)
-                if(i>=2){
-                    method[i] =Math.max(method[i], method[i-2]+a)
-                }
-                if(i>=5){
-                    method[i] =Math.min(method[i], method[i-5]+b)
-                }
-            }
-            for(var i=n+3; i>=1; i--){
-                method[i] = Math.min(method[i], method[i+1])
-            }
+function step11() {
+  var lines = ['4 110 200']
+  var ary = lines[0].split(' ')
+  var n = Number.parseInt(ary[0])
+  var a = Number.parseInt(ary[1])
+  var b = Number.parseInt(ary[2])
+  var method = []
+  method.push(0)
+  method.push(a)
+  if (n >= 1 && n <= 1000 && a >= 1 && a <= 10000 && b >= 1 && b <= 10000) {
+    if (a < b) {
+      for (var i = 2; i < n + 5; i++) {
+        method.push(0)
+        if (i >= 2) {
+          method[i] = Math.max(method[i], method[i - 2] + a)
         }
+        if (i >= 5) {
+          method[i] = Math.min(method[i], method[i - 5] + b)
+        }
+      }
+      for (var i = n + 3; i >= 1; i--) {
+        method[i] = Math.min(method[i], method[i + 1])
+      }
     }
-    console.log('STEP: 11 最安値を達成するには 2 ' +method[n])
+  }
+  console.log('STEP: 11 最安値を達成するには 2 ' + method[n])
 }
 step11()
 
@@ -282,42 +278,49 @@ step11()
  *  りんごの買い方を工夫したとき、最終的に n 個のりんごを手に入れるために必要な金額の最小値はいくらでしょうか。
  *  なお、買い方を工夫した結果、買ったりんごが n+1 個以上になってもよいものとします。
  */
-function step12(){
-    var lines = ['513 52 4492 593 9803']
-    var ary = lines[0].split(' ')
-    var n = Number.parseInt(ary[0])
-    var x = Number.parseInt(ary[1])
-    var a = Number.parseInt(ary[2])
-    var y = Number.parseInt(ary[3])
-    var b = Number.parseInt(ary[4])
-    var method = []
-    for(var i=0; i<x+n+y; i++) method.push(0)
-    if( (n>=1 && n<=1000) && 
-        (x>=1 && x<=1000) && 
-        (y>=1 && y<=1000) && 
-        (a>=1 && a<=10000) && 
-        (b>=1 && b<=10000)){
-        if(a<b){
-            for(var i=x; i<n+y; i++){
-                if(i>=x){
-                    method[i] =Math.max(method[i], method[i-x]+a)
-                }
-                if(i>=y){
-                    method[i] =Math.min(method[i], method[i-y]+b)
-                }
-            }
-            for(var i=n+(y-2); i>=1; i--){
-                method[i] = Math.min(method[i], method[i+1])
-            }
+function step12() {
+  var lines = ['513 52 4492 593 9803']
+  var ary = lines[0].split(' ')
+  var n = Number.parseInt(ary[0])
+  var x = Number.parseInt(ary[1])
+  var a = Number.parseInt(ary[2])
+  var y = Number.parseInt(ary[3])
+  var b = Number.parseInt(ary[4])
+  var method = []
+  for (var i = 0; i < x + n + y; i++) method.push(0)
+  if (
+    n >= 1 &&
+    n <= 1000 &&
+    x >= 1 &&
+    x <= 1000 &&
+    y >= 1 &&
+    y <= 1000 &&
+    a >= 1 &&
+    a <= 10000 &&
+    b >= 1 &&
+    b <= 10000
+  ) {
+    if (a < b) {
+      for (var i = x; i < n + y; i++) {
+        if (i >= x) {
+          method[i] = Math.max(method[i], method[i - x] + a)
         }
+        if (i >= y) {
+          method[i] = Math.min(method[i], method[i - y] + b)
+        }
+      }
+      for (var i = n + (y - 2); i >= 1; i--) {
+        method[i] = Math.min(method[i], method[i + 1])
+      }
     }
-    console.log(method[n])
+  }
+  console.log(method[n])
 }
 step12()
 
 /*
  * STEP: 13 最安値を達成するには 4
- *  
+ *
  */
 
 /*
@@ -327,29 +330,29 @@ step12()
  *  区間 [l, r] は背の順であると呼ぶことにします。また、区間 [l, r] の長さを r-l+1 とします。
  *  背の順であるような区間のうち、最長であるものの長さを出力してください。
  */
-function step14(){
-    var lines = [5, 160, 178, 170, 190, 190]
-    var n = Number.parseInt(lines[0])
-    
-    if(n>=1 && n<=200000){
-        if(n==1) {
-            console.log(1)
-        }else{
-            lines.shift()
-            var dp = []
-            dp.push(0)
-            for(var i=1; i<lines.length; i++){
-                var a_i = Number.parseInt(lines[i-1])
-                var a = Number.parseInt(lines[i])
-                if(a_i<=a){
-                    dp.push(dp[i-1]+1)
-                }else{
-                    dp.push(1)
-                }
-            }
-            console.log(Math.max(...dp))
+function step14() {
+  var lines = [5, 160, 178, 170, 190, 190]
+  var n = Number.parseInt(lines[0])
+
+  if (n >= 1 && n <= 200000) {
+    if (n == 1) {
+      console.log(1)
+    } else {
+      lines.shift()
+      var dp = []
+      dp.push(0)
+      for (var i = 1; i < lines.length; i++) {
+        var a_i = Number.parseInt(lines[i - 1])
+        var a = Number.parseInt(lines[i])
+        if (a_i <= a) {
+          dp.push(dp[i - 1] + 1)
+        } else {
+          dp.push(1)
         }
+      }
+      console.log(Math.max(...dp))
     }
+  }
 }
 step14()
 
@@ -360,33 +363,33 @@ step14()
  *  区間 [l, r] は逆背の順であると呼ぶことにします。また、区間 [l, r] の長さを r-l+1 とします。
  *  逆背の順であるような区間のうち、最長であるものの長さを出力してください。
  */
-function step15(){
-    var lines = [5, 190, 190, 170, 178, 164]
-    var n = Number.parseInt(lines[0])
-    
-    if(n>=1 && n<=200000){
-        if(n==1) {
-            console.log(1)
-        }else{
-            lines.shift()
-            var dp = []
-            dp.push(0)
-            for(var i=1; i<=lines.length; i++){
-                var a_i = Number.parseInt(lines[i-1])
-                var a = Number.parseInt(lines[i])
-                if(a_i>=a){
-                    dp.push(dp[i-1]+1)
-                }else{
-                    dp.push(1)
-                }
-            }
-            console.log(Math.max(...dp))               
+function step15() {
+  var lines = [5, 190, 190, 170, 178, 164]
+  var n = Number.parseInt(lines[0])
+
+  if (n >= 1 && n <= 200000) {
+    if (n == 1) {
+      console.log(1)
+    } else {
+      lines.shift()
+      var dp = []
+      dp.push(0)
+      for (var i = 1; i <= lines.length; i++) {
+        var a_i = Number.parseInt(lines[i - 1])
+        var a = Number.parseInt(lines[i])
+        if (a_i >= a) {
+          dp.push(dp[i - 1] + 1)
+        } else {
+          dp.push(1)
         }
+      }
+      console.log(Math.max(...dp))
     }
+  }
 }
 step15()
 
-/* 
+/*
  * STEP: 16 最長部分増加列
  *  n 本の木が横一列に並んでいます。左から i 番目の木を木 i と呼ぶことにします。木 i の高さは a_i [cm] です。
  *  あなたは、何本かの木を伐採することによって、残った木を左から順に見ると高さが単調増加になっているようにしたいと考えています。
@@ -394,21 +397,21 @@ step15()
  *  なるべく多くの木が残るように、伐採する木を工夫して選んだとき、伐採されずに残る木の本数が最大でいくつになるか求めてください。
  *  なお、最初から n 本の木が単調増加に並んでいる場合は、1本も伐採しなくてよいものとします。
  */
-function step16(){
-    var lines = [5, 100, 102, 101, 91, 199]
-    var n = Number.parseInt(lines[0])
-    lines.shift()
-    var dp = []
-    for(var i=0; i<n; i++) dp.push(0)
-    dp[1] = 1
-    for(i=0; i<n; i++){
-        dp[i] = 1
-        for(var j=0; j<i; j++){
-            if(lines[j]<lines[i]){
-                dp[i] = Math.max(dp[i], dp[j]+1)
-            }
-        }
+function step16() {
+  var lines = [5, 100, 102, 101, 91, 199]
+  var n = Number.parseInt(lines[0])
+  lines.shift()
+  var dp = []
+  for (var i = 0; i < n; i++) dp.push(0)
+  dp[1] = 1
+  for (i = 0; i < n; i++) {
+    dp[i] = 1
+    for (var j = 0; j < i; j++) {
+      if (lines[j] < lines[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
     }
-    console.log(Math.max(...dp))
+  }
+  console.log(Math.max(...dp))
 }
 step16()
--- src/newBLankLevelUp.js
+++ src/newBLankLevelUp.js
@@ -4,41 +4,130 @@
  *  盤面の与えられた座標の文字が "." の場合は "#" に、"#" の場合は "." に書き換えた後の盤面を出力してください。
  *  なお、マスの座標系は左上端のマスの座標を ( y , x ) = ( 0 , 0 ) とし、下方向が y 座標の正の向き、右方向が x 座標の正の向きとします。
  */
-function step1(){
-    //var lines = ['3 3', '...', '...', '...', '0 0']
-    var lines = ['4 4','####', '####', '....', '##..','2 2']
-    var ary = lines[0].split(' ')
-    var H = Number.parseInt(ary[0])
-    var W = Number.parseInt(ary[1])
-    lines.shift()
-    var board = []
-    var othello  = lines[lines.length-1].split(' ')
-    for(var i in othello){
-        othello[i] = Number.parseInt(othello[i])
-    }
-    for(var i=0; i<H; i++){
-        board.push(lines[i])
-    }
-   
-    for(var b in board){
-        var str = ''
-        if(othello[0] == b){
-            for(var j=0; j<board[b].length; j++){
-                if(othello[1] == j){
-                    var char = board[b].slice(othello[1],othello[1]+1)
-                    if(char == '.'){
-                        str = str + '#'
-                    }else{
-                        str = str + '.'
-                    }
-                }else{
-                    str = str + board[b].slice(j,j+1)
-                }
-            }
-        }else{
-            str = str + board[b]
+function step1() {
+  //var lines = ['3 3', '...', '...', '...', '0 0']
+  var lines = ['4 4', '####', '####', '....', '##..', '2 2']
+  var ary = lines[0].split(' ')
+  var H = Number.parseInt(ary[0])
+  var W = Number.parseInt(ary[1])
+  lines.shift()
+  var board = []
+  var othello = lines[lines.length - 1].split(' ')
+  for (var i in othello) {
+    othello[i] = Number.parseInt(othello[i])
+  }
+  for (var i = 0; i < H; i++) {
+    board.push(lines[i])
+  }
+
+  for (var b in board) {
+    var str = ''
+    if (othello[0] == b) {
+      for (var j = 0; j < board[b].length; j++) {
+        if (othello[1] == j) {
+          var char = board[b].slice(othello[1], othello[1] + 1)
+          if (char == '.') {
+            str = str + '#'
+          } else {
+            str = str + '.'
+          }
+        } else {
+          str = str + board[b].slice(j, j + 1)
         }
-        console.log('STEP: 1 ' + str)
+      }
+    } else {
+      str = str + board[b]
     }
+    console.log('STEP: 1 ' + str)
+  }
 }
 step1()
+
+/*
+ * STEP: 2 マップの扱い 2】マップの書き換え・縦横
+ *  マップを表す H 行 W 列の文字列 S_1 ... S_H と y , x 座標 が与えられるので、与えられた座標のマスと上下左右で隣接するマスの最大 5 マスについて
+ *  次の処理をおこなった後の盤面を出力してください。
+ *  ・ マスに書かれている文字が "." の場合は "#" に、"#" の場合は "." に書き換える。
+ *  なお、マスの座標系は左上端のマスの座標を ( y , x ) = ( 0 , 0 ) とし、下方向が y 座標の正の向き、右方向が x 座標の正の向きとします。
+ */
+function step2() {
+  //var lines = ['3 3', '##.', '###', '...', '1 1']
+  var lines = ['10 10','##########', '..........', '##########',
+                '##########', '..........', '#.#.#.#.#.', 
+                '.#.#.#.#.#', '#.#.#.#.#.', '.#.#.#.#.#',
+                '..........','2 3']
+  
+  var ary = lines[0].split(' ')
+  var H = Number.parseInt(ary[0])
+  var W = Number.parseInt(ary[1])
+  lines.shift()
+  
+  var coordinate = lines[lines.length - 1].split(' ')
+  var y = Number.parseInt(coordinate[0])
+  var x = Number.parseInt(coordinate[1])
+  var move = [-1,1]
+
+  var board = []
+  for (var i = 0; i <H; i++) {
+    board[i] = []
+    for(var j=0; j<lines[i].length; j++){
+      board[i][j] = lines[i].slice(j,j+1)
+    }
+  }
+  board.forEach(b=>{
+    var str = b.join()
+    str = str.replaceAll(',','')
+  })
+  for(i=0; i<board.length; i++){
+    for(var j=0; j<board[i].length; j++){
+      if(i==(y+move[0])){
+        if(j==x){
+          if(board[i][j]=='.'){
+            board[i][j] = '#'
+          }else{
+            board[i][j] = '.'
+          }
+        }
+      }
+      if(i==y){
+        if(j==(x+move[0])){
+          if(board[i][j]=='.'){
+            board[i][j] = '#'
+          }else{
+            board[i][j] = '.'
+          }
+        }
+        if(j==x){
+          if(board[i][j]=='.'){
+            board[i][j] = '#'
+          }else{
+            board[i][j] = '.'
+          }
+        }
+        if(j==(x+move[1])){
+          if(board[i][j]=='.'){
+            board[i][j] = '#'
+          }else{
+            board[i][j] = '.'
+          }
+        }
+      }
+      if(i==(y+move[1])){
+        if(j==x){
+          if(board[i][j]=='.'){
+            board[i][j] = '#'
+          }else{
+            board[i][j] = '.'
+          }
+        }
+      }
+    }
+  }
+  
+  board.forEach(b=>{
+    var str = b.join()
+    str = str.replaceAll(',','')
+    console.log('STEP: 2 ' + str)
+  })
+}
+step2()
